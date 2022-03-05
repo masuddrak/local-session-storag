310d0e4b91428dad1d6549f87969f64a3026fde2
masudrana <masudranaacb@gmail.com>
2022-03-05 23:07:35 +0600

    creat simple local storage

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const addBtn=()=>{
     const input=document.getElementById('inputCard')
     const name=input.value
-    console.log(name)
-    addIteams(name)
 
-    input.value=''
-    
+    addIteams(name)
+    productToCard(name)
+
+
+
+
+    input.value=''  
 }
 const addIteams=(name)=>{
     const addIteam=document.getElementById('addIteam')
     let li=document.createElement('li')
     li.innerText=name
     addIteam.appendChild(li)
+}
+// get local api
+const getCard=()=>{
+    const card=localStorage.getItem('card')
+    console.log(card)
+    let newObject
+    if(card){
+        newObject=JSON.parse(card) 
+    }
+    else{
+        newObject={}
+    }
+    return newObject
+}
+
+// add prpduct ot card
+const productToCard=name=>{
+    const card=getCard()
+    card[name]=1
+    const cardStrigifid=JSON.stringify(card)
+    localStorage.setItem('card',cardStrigifid)
 }
