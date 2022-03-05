b2132cecbe3c6b13a3901afd4c3795e5a4eef144
masudrana <masudranaacb@gmail.com>
2022-03-05 22:25:33 +0600

    add new item

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const addBtn=()=>{
     const input=document.getElementById('inputCard')
     const name=input.value
     console.log(name)
+    addIteams(name)
+
     input.value=''
+    
+}
+const addIteams=(name)=>{
+    const addIteam=document.getElementById('addIteam')
+    let li=document.createElement('li')
+    li.innerText=name
+    addIteam.appendChild(li)
 }
